47a11a206de864a9170193fac6bb5b7ad9babf40
Rodney Moreno <rodney.moreno@utexas.edu>
2020-08-11 16:57:13 -0500

    Initail server setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
+const http = require('http');
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.listen(port, () => console.log('Listening'));
+const clientPath = `${__dirname}/../client/public`;
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+server.on('error', function(err) {
+	console.error('Server error: ', err);
+});
+
+server.listen(8080, function() {
+	console.log('Listening on 8080');
+});
+
+

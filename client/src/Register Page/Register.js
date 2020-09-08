1159f6d6449b6f4234d46f83f11ac0595cd78e7f
Rodney Moreno <rodney.moreno@utexas.edu>
2020-09-08 08:22:20 -0500

    Fixed node modules and added basic form functionality

--- client/src/Register Page/Register.js
+++ client/src/Register Page/Register.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(props) {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
     
-    function handClick() {
-
+    function handleUserNameChange(event) {
+        setUserName(event.target.value);
     }
 
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
 
+    function handleSubmit() {
+        
+    }
 
     return(
         <div>
             <h2>Register</h2>
-            <form>
+            <form onSubmit = { handleSubmit }>
                 <label>username</label><br/>
-                <input type="text"></input><br/>
+                <input type="text" value = { username } onChange = { handleUserNameChange }></input><br/>
                 <label>password</label><br/>
-                <input type="text"></input><br/><br/>
+                <input type="text" value = { password } onChange = { handlePasswordChange }></input><br/><br/>
                 <input type="submit"></input>
             </form>
         </div>
